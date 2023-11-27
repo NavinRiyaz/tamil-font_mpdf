ce70f12ea364dbff8c5d0eb1c49f7b1c6c662e1b
NavinRiyaz <navinprakashronaldo@gmail.com>
2023-11-27 12:25:55 +0530

    First Commit

---
+++ index.php
@@ -0,0 +1,60 @@
+<?php
+
+// require composer autoload
+require_once __DIR__ . '/bootstrap.php';
+
+$defaultConfig = (new Mpdf\Config\ConfigVariables())->getDefaults();
+$fontDirs = $defaultConfig['fontDir'];
+
+$defaultFontConfig = (new Mpdf\Config\FontVariables())->getDefaults();
+$fontData = $defaultFontConfig['fontdata'];
+
+$mpdf = new \Mpdf\Mpdf([
+	'fontDir' => array_merge($fontDirs, [__DIR__]),
+	'fontdata' => $fontData + 
+		[
+			'angerthas' => [
+				'R' => 'assets/angerthas.ttf',
+			],
+			'inkfree' => [
+				'R' => 'assets/Inkfree.ttf',
+			],
+			'baamini' => [
+				'R' => 'assets/Baamini.ttf',
+			],
+		],
+]);
+
+$mpdf->WriteHtml('<html>
+    <head>
+		<style>
+		.inkfree {
+			font-family: "Ink Free";
+		}
+		.baamini {
+			font-family: "Baamini";
+		}
+		</style>
+    </head>
+    <body>
+<h1>Using custom font in the document</h1>
+
+<p style=\'font-family: angerthas\'>This example shows how to keep default font families while adding a custom font directory and definitions.</p>
+
+<p style="font-family: \'Ink Free\'">Inkfree line of text</p>
+
+<p style="font-family: "Ink Free";">Inkfree line of text that is not working</p>
+
+<p style=\'font-family: "Ink Free"\'>Inkfree line of text that is not working</p>
+
+<p class="inkfree">Inkfree line of text</p>
+
+<p style=\'font-family: "Baamini"\'>Inkfree line of text that is not working</p>
+
+<h1 class="baamini">muR jfty; bj</h1>
+
+</body>
+</html>');
+
+$mpdf->Output();
+die;
